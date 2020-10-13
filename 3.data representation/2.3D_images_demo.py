2ab14c6775432c70252385c2cc9525dbf517a8e0
Junenatte-and-little-pig <sy5622_5@126.com>
2020-10-13 08:05:28 +0800

    turn to imageio 4 3d images

--- 3.data representation/2.3D_images_demo.py
+++ 3.data representation/2.3D_images_demo.py
@@ -1,28 +1,24 @@
 # -*- encoding: utf-8 -*-
-import cv2 as cv
-import pydicom
-import os
-import matplotlib.pyplot as plt
+import imageio
+
+import torch
 
 
 def main():
-    ct=pydicom.dcmread('../data/p1ch4/volumetric-dicom/2-LUNG 3.0  B70f-04083/000000.dcm')
-    print(ct)
-    print(ct.pixel_array)
-    plt.figure(figsize=[10,10])
-    plt.imshow(ct.pixel_array,cmap=plt.bone())
-    plt.show()
+    # ct=pydicom.dcmread('../data/p1ch4/volumetric-dicom/2-LUNG 3.0  B70f-04083/000000.dcm')
+    # print(ct)
+    # print(ct.pixel_array)
+    # plt.figure(figsize=[10,10])
+    # plt.imshow(ct.pixel_array,cmap=plt.bone())
+    # plt.show()
 
-    # data_dir='../data/p1ch4/volumetric-dicom/2-LUNG 3.0  B70f-04083'
-    # filenames = [name for name in os.listdir(data_dir) if
-    #              os.path.splitext(name)[-1] == '.dcm']
-    # for filename in filenames:
-    #     ct=pydicom.dcmread(os.path.join(data_dir,filename))
-    #     plt.figure(figsize=[10, 10])
-    #     plt.imshow(ct.pixel_array, cmap=plt.bone())
-    #     plt.show()
-    #     plt.cla()
-    #     plt.close('all')
+    data_dir = '../data/p1ch4/volumetric-dicom/2-LUNG 3.0  B70f-04083'
+    vol_arr = imageio.volread(data_dir, 'DICOM')
+    print(vol_arr.shape)
+    vol=torch.from_numpy(vol_arr).float()
+    vol=torch.unsqueeze(vol,0)
+    print(vol.shape)
+    
 
 
 if __name__ == '__main__':
